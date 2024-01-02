a34224723421e53edfaea75e4c686c423b970657
zannam <146860952+marinazanoni@users.noreply.github.com>
2024-01-02 14:44:13 +0100

    feature1

--- feature1.py
+++ feature1.py
@@ -1,35 +1,43 @@
 # feature1.py
 
+# Import of required libraries
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
+# We define a class to get all the attribute "jointly"
 class GetFeature1:
 
+    # Our object is described by the graph itself and the name (will be unused)
     def __init__(self, graph, name):
         self.__graph = graph
         self.__name = name
 
+    # We get the number of nodes trough nx
     @property
     def nnodes(self):
         return self.__graph.number_of_nodes()
 
+    # We get the number of edges trough nx
     @property
     def nedges(self):
         return self.__graph.number_of_edges()
 
+    # We get the density of the graph as already implemented in nx
     @property
     def density(self):
         return nx.density(self.__graph)
 
+    # Calculate the degree distribution
     @property
     def degreedistr(self):
-        # Calculate the degree distribution
         degree_sequence = [d for n, d in self.__graph.degree()]
         # Visualize it
         plt.hist(degree_sequence, bins=5, density=True, alpha=0.7)
-        plt.show()  # Added to display the histogram
+        # Added to display the histogram
+        plt.show()
 
+    # Calculate the average degree as by formula using number of nodes and number od edges
     @property
     def averagedegree(self):
         average_degree = 2 * self.__graph.number_of_edges() / self.__graph.number_of_nodes()
